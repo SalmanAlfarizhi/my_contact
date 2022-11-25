4e39ed0e1720a880157c79e6ba6616d3717b6c6c
itsmekliwon <alfarizhisalman13@gmail.com>
2022-11-25 22:41:22 +0700

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(90)),
+                color: Color.fromARGB(255, 149, 83, 241),
+              ),
+              child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: Image.asset(
+                          "images/logoclean.png"
+                        ),
+                      ),
+                    ],
+                  )
+              ),
+            ),
+            SizedBox(height: 50),
+            Text("Welcome Back!",
+            style: TextStyle(
+              fontSize: 24,
+              fontFamily: 'PromptBold',
+              color: Color.fromARGB(255, 149, 83, 241),
+            ),),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(left: 20, right: 20, top: 50),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              height: 54,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.grey[200],
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 50,
+                      color: Color(0xffEEEEEE)
+                  ),
+                ],
+              ),
+              child: TextField(
+                cursorColor: Color.fromARGB(255, 149, 83, 241),
+                decoration: InputDecoration(
+                  icon: Icon(
+                    Icons.email,
+                    color: Color.fromARGB(255, 149, 83, 241),
+                  ),
+                  hintText: "Enter Email",hintStyle: TextStyle(
+                    fontFamily: 'PromptRegular',
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 149, 83, 241),
+                  ),
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                ),
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              height: 54,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Color(0xffEEEEEE),
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 20),
+                      blurRadius: 100,
+                      color: Color(0xffEEEEEE)
+                  ),
+                ],
+              ),
+              child: TextField(
+                cursorColor: Color.fromARGB(255, 149, 83, 241),
+                decoration: InputDecoration(
+                  focusColor: Color.fromARGB(255, 149, 83, 241),
+                  icon: Icon(
+                    Icons.vpn_key,
+                    color: Color.fromARGB(255, 149, 83, 241),
+                  ),
+                  hintText: "Enter Password",hintStyle: TextStyle(
+                    fontFamily: 'PromptRegular',
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 149, 83, 241),
+                  ),
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              alignment: Alignment.centerRight,
+              child: GestureDetector(
+                onTap: () {
+                  // Write Click Listener Code Here
+                },
+                child: Text("Forgot Password?",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 149, 83, 241),
+                  fontSize: 16,
+                  fontFamily: 'PromptRegular'
+                ),),
+              ),
+            ),
+
+            GestureDetector(
+              // onTap: () {
+              //   Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()),
+              //   );
+              // },
+              child: Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(left: 20, right: 20, top: 70),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                height: 54,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 149, 83, 241),
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Color(0xffEEEEEE)
+                    ),
+                  ],
+                ),
+                child: Text(
+                  "Sign in",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontFamily: 'PromptMedium'
+                  ),
+                ),
+              ),
+            ),
+
+            GestureDetector(
+              // onTap: () {
+              //   Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterPage()),
+              //   );
+              // },
+              child: Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                height: 54,
+                decoration: BoxDecoration(
+                  color: Color(0xFFF0F0),
+                  borderRadius: BorderRadius.circular(50),
+                  border: Border.all(
+                    color: Color.fromARGB(255, 149, 83, 241,
+                    ),
+                    width: 1,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Color(0xffEEEEEE)
+                    ),
+                  ],
+                ),
+                child: Text(
+                  "Sign up",
+                  style: TextStyle(
+                    fontFamily: 'PromptMedium',
+                    fontSize: 18,
+                      color: Color.fromARGB(255, 149, 83, 241,)
+                  ),
+                ),
+              ),
+            ),
+
+          ],
+        )
+      )
+    );
+  }
+}
---
+++ lib/pages/splash.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:my_contact/pages/login_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+    void initState(){
+    super.initState();
+    startTimer();
+  }
+
+  startTimer() async {
+    var duration = Duration(seconds: 5);
+    return new Timer(duration, LoginRoute);
+  }
+
+  LoginRoute(){
+    Navigator.pushReplacement(context, MaterialPageRoute(builder:(context) => LoginPage()));
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+         color: Color.fromARGB(255, 149, 83, 241),
+        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/logomycontact.png'),
+          // Text("#oneforsolution",
+          // style: TextStyle(
+          //   fontSize: 20,
+          //   color: Colors.white
+          // ),
+          // )
+        ],
+      ),
+      ),
+    );
+  }
+}
